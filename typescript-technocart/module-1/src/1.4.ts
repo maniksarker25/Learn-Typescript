0fe4621856b653e2d5a44a4f112d60be78224a49
Manik <tonysins43@gmail.com>
2023-11-02 00:12:08 +0600

    configure typescript

---
+++ typescript-technocart/module-1/src/1.4.ts
@@ -0,0 +1,18 @@
+// basic data type 
+
+//string
+let firstName : string = "manik";
+//number
+let roll : number = 125;
+// boolean 
+let isStudent : boolean = true;
+
+// undefined
+let x : undefined = undefined;
+//null
+let y : null = null;
+
+// 
+let id : number;
+
+id = 1232;
