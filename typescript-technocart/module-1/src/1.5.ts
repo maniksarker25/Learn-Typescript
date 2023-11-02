c3cfd2b28a84b1e94ef58e84689b6438580047db
Manik <tonysins43@gmail.com>
2023-11-02 11:09:52 +0600

    learn typescript with function

---
+++ typescript-technocart/module-1/src/1.5.ts
@@ -0,0 +1,19 @@
+// reference type -> object
+// optional type -> literal type
+
+let user : {
+    company:"Programming Hero"; // literal type
+   readonly location:string;  // you can not change it like user.location = "india"
+    firstName:string;
+    middleName?:string; // optional type 
+    lastName:string;
+    isMarried:boolean;
+} = {
+    company:"Programming Hero",
+    location:"Bangladesh",
+    firstName:"Manik",
+    middleName:"chandra",
+    lastName : "Sarker",
+    isMarried:false
+}
+
---
+++ typescript-technocart/module-1/src/1.6.ts
@@ -0,0 +1,27 @@
+// learning function
+
+// normal function
+
+function add(num1: number, num2: number): number {
+  return num1 + num2;
+}
+add(20, 20);
+
+// arrow function
+
+const sum = (num1: number, num2: number): number => num1 + num2;
+
+// object -> function ->------method
+
+const poorUser = {
+  name: "manik sarker",
+  balance: 0,
+  addBalance(balance: number): string {
+    return `My new balance is ${this.balance + balance}`;
+  },
+};
+
+
+// array map 
+const arr : number[] = [2,4,5,6];
+const newArray : number[] = arr.map((elem :number): number =>elem * elem)
