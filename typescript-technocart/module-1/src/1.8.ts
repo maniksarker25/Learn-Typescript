476a78ab5a4206e301abfb47aa277271fa28b9ba
Manik <tonysins43@gmail.com>
2023-11-02 12:07:14 +0600

    learn ts with destructuring

--- typescript-technocart/module-1/src/1.8.ts
+++ typescript-technocart/module-1/src/1.8.ts
@@ -1,1 +1,20 @@
 // destructuring----------------
+
+
+// object destructuring-----------
+const person = {
+ id :215,
+ name:{
+    firstName:"manik",
+    middleName:"sarker",
+    last:"sarker"
+ },
+ address:"bangladesh",
+ contact:"017000000"
+}
+
+const {contact,name:{middleName}} = person;
+
+// array destructuring----------
+const myFriends : string[] = ["omi","oysi",'naha','himu','sadia'];
+const [,, bestFriend, ...rest] = myFriends;
