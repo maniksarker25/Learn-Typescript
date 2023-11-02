847655e26a626d928035b18ef39ec4d791429a0f
Manik <tonysins43@gmail.com>
2023-11-02 12:33:42 +0600

    learn type alias

--- typescript-technocart/module-1/src/1.9.ts
+++ typescript-technocart/module-1/src/1.9.ts
@@ -1,2 +1,40 @@
 // learn type alias --------------------
 
+
+// declare type --------------------
+type Student = {
+    name:string;
+    age:number;
+    gender:string;
+    address:string;
+    contactNo?:string
+}
+
+const student1 :Student={
+    name:"manik sarker",
+    age:20,
+    gender:"male",
+    address:"Bangladesh",
+    contactNo:"01800000000"
+}
+
+const student2 : Student = {
+    name:"asikur",
+    age:22,
+    gender:"male",
+    address:"Jashore"
+}
+
+/// 
+type UserName = string;
+type IsAdmin = boolean;
+
+const userName : UserName = "manik";
+const isAdmin : IsAdmin = true;
+
+
+// type alias with function --------------------------------
+
+type Add = (num1:number,num2:number) => number;
+
+const add2Number : Add = (num1,num2) => num1 + num2;
