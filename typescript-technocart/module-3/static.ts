e5ebf9b4cb25e2c11f3d466c9cdc6ca829ff935f
Manik <tonysins43@gmail.com>
2023-11-06 09:45:47 +0600

    learn static

--- typescript-technocart/module-3/static.ts
+++ typescript-technocart/module-3/static.ts
@@ -1,7 +1,21 @@
 {
+  // Learn static -------------------------
 
+  class Counter {
+    static count: number = 0;
+   static increment() {
+      return (Counter.count = Counter.count + 1);
+    }
+   static decrement() {
+      return (Counter.count = Counter.count - 1);
+    }
+  }
 
-
-
-    
+//   const instance1 = new Counter();
+  console.log(Counter.increment()); // different memory ------
+  console.log(Counter.increment());
+//   const instance2 = new Counter();
+  console.log(Counter.increment()); // different memory -----------
+  console.log(Counter.increment());
+  //
 }
